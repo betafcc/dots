3c8bb489588af6f867310cb6721ea2f88fad5223
Beta Faccion <betafcc@gmail.com>
2018-06-16 12:32:18 -0300

    little clean up

--- installs.sh
+++ installs.sh
@@ -3,8 +3,6 @@ sudo apt update
 sudo apt install software-properties-common -y
 
 
-
-
 # install termite
 curl https://raw.githubusercontent.com/Corwind/termite-install/master/termite-install.sh | sh
 rm -rf termite vte-ng
@@ -22,7 +20,6 @@ wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key ad
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt update
 sudo apt install google-chrome-stable
-
 
 
 # install elementary tweaks
