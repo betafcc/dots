5b45d3cfe676ce2e961d42372d04e0c72e0d5a65
Beta Faccion <betafcc@gmail.com>
2018-06-16 14:12:53 -0300

    small cleanup

--- installs.sh
+++ installs.sh
@@ -23,8 +23,8 @@ sudo apt install sublime-text -y
 
 
 # install chrome
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+wget -qO - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
 sudo apt update
 sudo apt install google-chrome-stable
 
