730bf7254b87167bd18866a81269dfed98f2639c
Beta Faccion <betafcc@gmail.com>
2018-06-16 12:49:20 -0300

    adds emacs

--- installs.sh
+++ installs.sh
@@ -8,6 +8,12 @@ curl https://raw.githubusercontent.com/Corwind/termite-install/master/termite-in
 rm -rf termite vte-ng
 
 
+# install emacs
+sudo add-apt-repository ppa:ubuntu-elisp/ppa
+sudo apt update
+sudo apt install emacs25
+
+
 # install sublime text 3
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
