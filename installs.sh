5515a2aded1fa6e38f10de18945496ae29a898eb
Beta Faccion <betafcc@gmail.com>
2018-06-16 12:31:49 -0300

    adds more installs

--- installs.sh
+++ installs.sh
@@ -10,9 +10,22 @@ curl https://raw.githubusercontent.com/Corwind/termite-install/master/termite-in
 rm -rf termite vte-ng
 
 
+# install sublime text 3
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text -y
+
 
 # install chrome
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt update
 sudo apt install google-chrome-stable
+
+
+
+# install elementary tweaks
+sudo add-apt-repository ppa:philip.scott/elementary-tweaks
+sudo apt update
+sudo apt install elementary-tweaks -y
