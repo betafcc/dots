4a75cce0e3c3e2251beff3ad225fde20b135dec4
Beta Faccion <betafcc@gmail.com>
2018-06-17 04:32:37 -0300

    fix php instalation

--- installs.sh
+++ installs.sh
@@ -74,6 +74,28 @@ nvm install node
 
 
 # sets up php
+sudo apt install -y \
+    php7.0 \
+    php7.0-curl \
+    php7.0-json \
+    php7.0-cgi \
+    php7.0-fpm \
+    autoconf \
+    automake \
+    libxml2-dev \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    openssl \
+    gettext \
+    libicu-dev \
+    libmcrypt-dev \
+    libmcrypt4 \
+    libbz2-dev \
+    libreadline-dev \
+    build-essential \
+    libmhash-dev \
+    libmhash2 \
+    libxslt1-dev
 curl -L -O https://github.com/phpbrew/phpbrew/raw/master/phpbrew
 chmod +x phpbrew
 sudo mv phpbrew /usr/local/bin/phpbrew
