812d8e42dd7d84b7730bc1fabff9861acf696d0b
Beta Faccion <betafcc@gmail.com>
2018-07-28 18:38:15 -0300

    docker-compose

--- installs.sh
+++ installs.sh
@@ -69,6 +69,10 @@ sudo apt install dconf-tools
 # install docker
 curl -fsSL get.docker.com | sudo sh
 sudo usermod -aG docker "$USER"
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.22.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+exec "$SHELL"
 
 
 # sets up python
