a4d0c664768a98f46d9bffc2a62d42be783d2c4d
Beta Faccion <betafcc@gmail.com>
2018-12-22 00:53:45 -0200

    Adds touchp command

--- bash/lib/os_utils.sh
+++ bash/lib/os_utils.sh
@@ -10,6 +10,19 @@
 }
 
 
+# creates parent directory before touching file
+@touchp() {
+    local file_path
+    local file_dir
+    for file_path in "$@"
+    do
+        file_dir=$(dirname "${file_path}")
+        mkdir -p "${file_dir}"
+        touch "${file_path}"
+    done
+}
+
+
 @toggle() {
     if [ $(ps cax | grep "$1" | wc -l) -gt 0 ]
     then
