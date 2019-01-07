4850a1e509ed104a1986ec67b755cbdca4e1190a
Beta Faccion <betafcc@gmail.com>
2019-01-07 18:56:11 -0200

    adds basic import mecanism

--- bash/prelude.sh
+++ bash/prelude.sh
@@ -1,14 +1,106 @@
 __BASHRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+__PRELUDE_SRC="${__BASHRC_DIR}/"$(basename "${BASH_SOURCE[0]}")""
 
 
-prelude::is_interactive() {
+
+prelude::import() {
+    local reference
+    local name
+
+
+    if [[ ! -n "${1}" || "${1}" = "help"  ]]; then
+      echo  "Usage:
+    prelude::import <library_reference> [as <name>] [exposing ['..'| ...functions]]
+
+Examples:
+    prelude::import './foo/bar.sh'
+        imports from relative path as 'bar'
+
+    prelude::import './foo/bar.sh' as mod
+        imports from relative path as 'mod'
+
+    prelude::import './foo/bar.sh' exposing f g h
+        imports from relative path setting alias for f g and h
+
+    prelude::import './foo/bar.sh' exposing ..
+        imports from relative path and also sources it"
+
+      return 1
+    fi
+
+    reference="${1}"
+    shift
+
+
+    case "${1}" in
+        as)
+            shift
+            if [ -n "${1}" ]; then
+                name="${1}"
+            else
+                return 1
+            fi
+            shift
+        ;;
+
+        "" | exposing)
+            name="$(prelude::path::parse name "${reference}")"
+            ;;
+
+        *)
+            return 1
+            ;;
+    esac
+
+
+    eval "
+        ${name}() (
+            source '"${reference}"'
+            "'"$@"'"
+        )
+    "
+
+
+    if [ "${1}" = "exposing" ]; then
+        shift
+        if [ $# -eq 1 && "${1}" = ".." ]; then
+            source "${reference}"
+        else
+            for el in "$@"; do
+                eval "alias "${el}"="${name}" "${el}""
+            done
+        fi
+    fi
+}
+
+
+alias +import="prelude::import"
+
+
+prelude::process::is_interactive() {
     [[ $- == *i* ]]
 }
 
 
-prelude::path::source() {
+prelude::process::is_shell() {
+    [ "$(prelude::path::last_source)" = "${__PRELUDE_SRC}" ]
+}
+
+
+prelude::path::last_source() {
     local _dir="$( cd "$( dirname "${BASH_SOURCE[-1]}" )" >/dev/null && pwd )"
     local _full="${_dir}/"$(basename "${BASH_SOURCE[-1]}")""
+    echo "${_full}"
+}
+
+
+prelude::path::source() {
+    if prelude::process::is_shell; then
+        (>&2 echo "Can't get source file path from shell")
+        return 1
+    fi
+
+    local _full="$(prelude::path::last_source)"
 
     if [ $# -eq 0 ]; then
         echo "${_full}"
@@ -43,7 +135,7 @@ prelude::path::parse() {
             echo "    └────────────────┴──────┴─────┘"
             ;;
         *)
-            if prelude::is_interactive; then
+            if prelude::process::is_interactive; then
                 (>&2
                  echo "'${FUNCNAME[0]} ${1}' invalid"
                  prelude::path::parse help
