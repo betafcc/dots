67728e8068eb803d7293ec0042b537deb2388b0e
Beta Faccion <betafcc@gmail.com>
2019-01-08 17:01:41 -0200

    small refactoring and renaming

--- bash/prelude.sh
+++ bash/prelude.sh
@@ -2,124 +2,126 @@ __BASHRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 __PRELUDE_SRC="${__BASHRC_DIR}/"$(basename "${BASH_SOURCE[0]}")""
 
 
+{
+    ::import() {
+        local reference
+        local name
+        local resolved_reference
 
-prelude::import() {
-    local reference
-    local name
-    local resolved_reference
-
-    if [[ ! -n "${1}" || "${1}" = "help"  ]]; then
-      echo  "Usage:
-    prelude::import <library_reference> [as <name>] [exposing ['..'| ...functions]]
+        if [[ ! -n "${1}" || "${1}" = "help" ]]; then
+            echo  "Usage:
+    ::import <library_reference> [as <name>] [exposing ['..'| ...functions]]
 
 Examples:
-    prelude::import './foo/bar.sh'
+    ::import './foo/bar.sh'
         imports from relative path as 'bar'
 
-    prelude::import './foo/bar.sh' as mod
+    ::import './foo/bar.sh' as mod
         imports from relative path as 'mod'
 
-    prelude::import './foo/bar.sh' exposing f g h
+    ::import './foo/bar.sh' exposing f g h
         imports from relative path setting alias for f g and h
 
-    prelude::import './foo/bar.sh' exposing ..
+    ::import './foo/bar.sh' exposing ..
         imports from relative path and also sources it"
 
-      return 1
-    fi
-
-    reference="${1}"
-    shift
-
-    case "${1}" in
-        as)
-            shift
-            if [ -n "${1}" ]; then
-                name="${1}"
-            else
-                return 1
-            fi
-            shift
-        ;;
-
-        "" | exposing)
-            name="$(prelude::path::parse name "${reference}")"
-            ;;
-
-        *)
             return 1
-            ;;
-    esac
-
-
-    # resolve reference last, cause it's the heaviest operation
-    if [[ "${reference:0:1}" = "/" ]]; then
-        resolved_reference="${reference}"
-    elif [[ "${reference:0:1}" = "." ]]; then
-        if prelude::process::is_shell; then
-            resolved_reference="$(pwd)/${reference}"
-        else
-            resolved_reference="$(dirname "${BASH_SOURCE[1]}")/${reference}"
         fi
-    else
-        return 1
-    fi
 
-    eval "
+        reference="${1}"
+        shift
+
+        case "${1}" in
+            as)
+                shift
+                if [ -n "${1}" ]; then
+                    name="${1}"
+                else
+                    return 1
+                fi
+                shift
+                ;;
+
+            "" | exposing)
+                name="$(::path::parse name "${reference}")"
+                ;;
+
+            *)
+                return 1
+                ;;
+        esac
+
+
+        # resolve reference last, cause it's the heaviest operation
+        if [[ "${reference:0:1}" = "/" ]]; then
+            resolved_reference="${reference}"
+        elif [[ "${reference:0:1}" = "." ]]; then
+            if ::process::is_shell; then
+                resolved_reference="$(pwd)/${reference}"
+            else
+                resolved_reference="$(dirname "${BASH_SOURCE[1]}")/${reference}"
+            fi
+        else
+            return 1
+        fi
+
+        eval "
         ${name}() (
             source "${resolved_reference}"
             "'"$@"'"
         )
+
+        ${name}::__file__() {
+            echo "${resolved_reference}"
+        }
     "
 
-    if [ "${1}" = "exposing" ]; then
-        shift
-        if [[ $# -eq 1 && "${1}" = ".." ]]; then
-            source "${resolved_reference}"
-        else
-            for el in "$@"; do
-                eval "${el}() { "${name}" "${el}" "'"$@"'"; }"
-            done
+        if [ "${1}" = "exposing" ]; then
+            shift
+            if [[ $# -eq 1 && "${1}" = ".." ]]; then
+                source "${resolved_reference}"
+            else
+                for el in "$@"; do
+                    eval "${el}() { "${name}" "${el}" "'"$@"'"; }"
+                done
+            fi
         fi
-    fi
+    }
 }
 
 
-alias +import="prelude::import"
-
-
 {
-    prelude::process::is_interactive() {
+    ::process::is_interactive() {
         [[ $- == *i* ]]
     }
 
 
-    prelude::process::is_shell() {
-        [ "$(prelude::path::last_source)" = "${__PRELUDE_SRC}" ]
+    ::process::is_shell() {
+        [ "$(::path::last_source)" = "${__PRELUDE_SRC}" ]
     }
 }
 
 
 {
-    prelude::path::is_absolute() {
+    ::path::is_absolute() {
         [ "${1:0:1}" = "/" ]
     }
 
 
-    prelude::path::last_source() {
+    ::path::last_source() {
         local _dir="$( cd "$( dirname "${BASH_SOURCE[-1]}" )" >/dev/null && pwd )"
         local _full="${_dir}/"$(basename "${BASH_SOURCE[-1]}")""
         echo "${_full}"
     }
 
 
-    prelude::path::parse() {
+    ::path::parse() {
         case "${1}" in
             dir) dirname "${2}";;
             base) basename "${2}";;
             ext) echo "${2##*.}";;
             name)
-                local _base="$(prelude::path::parse base "${2}")"
+                local _base="$(::path::parse base "${2}")"
                 echo "${_base%.*}"
                 ;;
             help)
@@ -138,10 +140,10 @@ alias +import="prelude::import"
                 echo "    └────────────────┴──────┴─────┘"
                 ;;
             *)
-                if prelude::process::is_interactive; then
+                if ::process::is_interactive; then
                     (>&2
                      echo "'${FUNCNAME[0]} ${1}' invalid"
-                     prelude::path::parse help
+                     ::path::parse help
                     )
                 fi
                 return 127;;
