56a4cf69ae82b367be0bbfc17556a9d04a4e9b3d
Beta Faccion <betafcc@gmail.com>
2023-12-19 05:25:13 +0100

    changes

---
+++ deno/util.ts
@@ -0,0 +1,10 @@
+export const post = (url: string, data: any, headers?: any) =>
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+    redirect: 'follow',
+    body: JSON.stringify(data),
+  })
